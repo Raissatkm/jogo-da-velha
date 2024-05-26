84da428e1e7a8ba7da183232a5a231518a403d0a
Álefe Cruz <98561479+Raissatkm@users.noreply.github.com>
2024-05-25 21:43:34 -0300

    .

--- jogo.py
+++ jogo.py
@@ -1,17 +1,11 @@
-def criarBoard():
-    pass
-
-def printBoard():
-    pass
-
-def  getInputValido():
-    pass
-
-def verificaMovimento():
-    pass
-
-def fazMovimento():
-    pass
-
-def verificaGanhador():
-    pass
+class JogodaVelha():
+    def __init__(self):
+        self.tabuleiro = [" " for _ in range(9)]
+        self.jogador_atual = "X"
+    
+    
+    def mostrar_tabuleiro(self):
+        for i in range(3):
+            print(f"{self.tabuleiro[3*i]} | {self.tabuleiro[3*i+ 1]} | {self.tabuleiro[3*i +2]}")
+            if i < 2:
+                print("--+---+--")
