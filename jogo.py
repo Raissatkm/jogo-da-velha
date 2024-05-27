a453863c82d162de6c9661c3728341391ec945a7
Álefe Cruz <98561479+Raissatkm@users.noreply.github.com>
2024-05-26 23:08:12 -0300

    .

--- jogo.py
+++ jogo.py
@@ -3,14 +3,15 @@ class JogoDaVelha:
         self.tabuleiro = [' ' for _ in range(9)]
         self.jogador_atual = 'X'
 
-    def mostrar_tabuleiro(self):
+    def exibir_tabuleiro(self):
         for i in range(3):
             print(f"{self.tabuleiro[3 * i]} | {self.tabuleiro[3 * i + 1]} | {self.tabuleiro[3 * i + 2]}")
             if i < 2:
                 print("--+---+--")
+                
 
     def jogar(self):
-        self.mostrar_tabuleiro()  
+        self.exibir_tabuleiro()  
         while True:
             print(f"Jogador {self.jogador_atual}, escolha uma posição (0-8): ", end=" ")
             try:
@@ -18,12 +19,14 @@ class JogoDaVelha:
                 if posicao < 0 or posicao > 8 or self.tabuleiro[posicao] != ' ':
                     print("Posição inválida. Tente novamente.")
                     continue
+                
 
                 self.tabuleiro[posicao] = self.jogador_atual
-                self.mostrar_tabuleiro()  
-                if self.verificar_vencedor():
+                self.exibir_tabuleiro()  
+                if self.checar_vencedor():
                     print(f"Parabéns! Jogador {self.jogador_atual} venceu!")
                     break
+                
 
                 if ' ' not in self.tabuleiro:
                     print("Empate! O tabuleiro está cheio.")
@@ -33,7 +36,8 @@ class JogoDaVelha:
             except ValueError:
                 print("Entrada inválida. Por favor, insira um número.")
 
-    def verificar_vencedor(self):
+
+    def checar_vencedor(self):
         combinacoes_vencedoras = [
             (0, 1, 2), (3, 4, 5), (6, 7, 8),  
             (0, 3, 6), (1, 4, 7), (2, 5, 8),  
